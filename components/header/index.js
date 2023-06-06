276686c07d3d938582800ae45e4edebc655c9777
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-06-06 17:59:04 +0200

    maj

--- components/header/index.js
+++ components/header/index.js
@@ -1,13 +1,14 @@
 function header(){
     return(
         <header>
-            <h1>Xavier Marcotte</h1>
+            <img src="pdp.jpg"></img>
+            {/* <h1>Xavier Marcotte</h1> */}
             <nav>
                 <a href="">Accueil</a>
                 <a href="">Projets Perso</a>
                 <a href="">Compétences</a>
                 <a href="">Apprentissage</a>
-                <a href="">Me contacter</a>
+                <a href="" className="contact">Me contacter</a>
             </nav>
         </header>
     )
