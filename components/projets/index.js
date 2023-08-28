1642ef4895e5f1387703f59f77c0d50688968919
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-08-28 10:26:05 +0200

    test deploiement

--- components/projets/index.js
+++ components/projets/index.js
@@ -7,7 +7,6 @@ function projets(){
                     {/* faire une boucle, mettre en ligne les projets et les importer ici (lien qui renvoie vers publications vercel) je pourrais ranger tout ça dans un tableau(plus simple) */}
                     {/* Je pourrais mettre l'api react de fin de saison, le projet de cohérence cardiaque et le pilori là où on y a touché pour la dernière fois (probablement après le côté back) */}
                 </div>
-                
             </div>
         </section>
     )
