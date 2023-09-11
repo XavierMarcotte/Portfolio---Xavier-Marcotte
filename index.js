3e0b31dec6b2002788905bea39964de27a39bc3d
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-09-11 11:11:48 +0200

    maj responsive contact, modifs pres

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App/index.js'; 
 
 
-
 const rootDOMElement = document.querySelector('.app');
 const root = createRoot(rootDOMElement);
 root.render(<App />);
